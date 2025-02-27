cb57f5dbd2a47fdb37affeb20a856315880aba27
Raizn51 <Gouravraikwar989@gmail.com>
2025-02-27 16:05:03 +0530

    [Gourav Raikwar] Add . the Logging Levels, Logging to Console or File, Logging Patterns, etc based on this application running in Dev, Staging or Production

--- src/main/java/com/spring/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/spring/employeepayrollapp/EmployeePayrollApplication.java
@@ -2,6 +2,8 @@ package com.spring.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
@@ -9,8 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-		log.info("Employee Payroll Application Started");
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll Application Started in {} Environment",context.getEnvironment().getProperty("environment"));
 
 	}
 }
